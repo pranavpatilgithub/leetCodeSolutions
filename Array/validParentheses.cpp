9ce26dcba3caed422508c326ec9c259bad3597e4
Pranav Patil <118053113+pranavpatilgithub@users.noreply.github.com>
2024-06-18 22:15:20 +0530

    Create validParentheses.cpp

---
+++ Array/validParentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        for(char ch : str){
+            if(ch == '(' || ch == '{' || ch == '['){
+                s.push(ch);
+            }
+            else{
+                if(s.empty() || 
+                   (ch == ')' && s.top() != '(') ||
+                   (ch == '}' && s.top() != '{') ||
+                   (ch == ']' && s.top() != '[') ){
+                    return false;
+                   }
+                   s.pop();
+            }
+        }
+        return s.empty();
+    }
+};
